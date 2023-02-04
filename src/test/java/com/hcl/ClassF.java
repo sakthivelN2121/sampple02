796369be2ab8e195794630934d884352e0032612
sakthivelN2121 <savysakthi2106@gmail.com>
2023-02-04 11:00:43 +0530

    new push

---
+++ src/test/java/com/hcl/ClassF.java
@@ -0,0 +1,22 @@
+package com.hcl;
+
+public class ClassF {
+	
+	public void test1() {
+
+		System.out.println("Test1");
+
+	}
+	
+	public void test2() {
+		
+		System.out.println("Test2");
+
+	}
+	public void test3() {
+		
+		System.out.println("Test3");
+
+	}
+
+}
